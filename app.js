1288c85fdacebfd07176799bb92a9ebf75615bea
giboow <philippe.gibert@gmail.com>
2013-10-07 22:51:02 +0200

    update

--- app.js
+++ app.js
@@ -8,11 +8,15 @@ var express = require('express')
 var app = express();
 app.set('config', config);
 
+
+
 module = require('./configs/module.js')(app);
 
 require('./configs/environment.js')(app, express);
 require('./configs/routes.js')(app);
 
+
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
